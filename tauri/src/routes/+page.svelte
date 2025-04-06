94d73650dfbb3d6a0407bd44e0c8f391b0c68df5
ugai <ugai.sub1@gmail.com>
2025-04-06 15:37:49 +0900

    Refactor: Tauri

--- tauri/src/routes/+page.svelte
+++ tauri/src/routes/+page.svelte
@@ -1,30 +1,63 @@
 <script lang="ts">
+  import type { PhysicalPosition } from "@tauri-apps/api/dpi";
   import { type UnlistenFn } from "@tauri-apps/api/event";
   import { getCurrentWebview } from "@tauri-apps/api/webview";
   import { onDestroy, onMount } from "svelte";
 
-  let dndState: "waiting" | "dragging" | "dropped" = $state("waiting");
+  let dropArea: HTMLDivElement | null = $state(null);
+  let dndState: "idle" | "dragOver" | "dropped" = $state("idle");
   let droppedPaths: string[] = $state([]);
+
+  let scaleFactor: number;
+  const hitTest = (
+    position: PhysicalPosition,
+    dropArea: HTMLDivElement | null
+  ): boolean => {
+    if (!dropArea) return false;
+    const lpos = position.toLogical(scaleFactor);
+    const r = dropArea.getBoundingClientRect();
+    const isInside =
+      lpos.x >= r.left &&
+      lpos.x <= r.right &&
+      lpos.y >= r.top &&
+      lpos.y <= r.bottom;
+    return isInside;
+  };
 
   const unlistens: UnlistenFn[] = [];
   onMount(async () => {
-    let unlisten: UnlistenFn;
-    unlisten = await getCurrentWebview().onDragDropEvent((event) => {
-      if (event.payload.type === "over") {
-        dndState = "dragging";
-        console.log("User hovering", event.payload.position);
-      } else if (event.payload.type === "drop") {
-        dndState = "dropped";
-        console.log("User dropped", event.payload.paths);
-        const paths = event.payload.paths;
-        if (paths && paths.length > 0) {
-          droppedPaths = paths;
-        }
-      } else {
-        dndState = "waiting";
-        console.log("File drop cancelled");
-      }
+    const webview = getCurrentWebview();
+    const window = webview.window;
+    let _unlisten: UnlistenFn;
+
+    // Watch DPI scale factor changes
+    scaleFactor = await window.scaleFactor();
+    _unlisten = await window.onScaleChanged((event) => {
+      scaleFactor = event.payload.scaleFactor;
     });
+    unlistens.push(_unlisten);
+
+    _unlisten = await webview.onDragDropEvent((event) => {
+      if (event.payload.type === "over") {
+        dndState = hitTest(event.payload.position, dropArea)
+          ? "dragOver"
+          : "idle";
+        return;
+      } else if (event.payload.type === "drop") {
+        dndState = hitTest(event.payload.position, dropArea)
+          ? "dropped"
+          : "idle";
+        if (dndState === "dropped") {
+          const paths = event.payload.paths;
+          if (paths && paths.length > 0) {
+            droppedPaths = paths;
+          }
+        }
+        return;
+      }
+      dndState = "idle";
+    });
+    unlistens.push(_unlisten);
   });
 
   onDestroy(() => {
@@ -35,23 +68,26 @@
 
   const clearDroppedPathss = () => {
     droppedPaths = [];
-    dndState = "waiting";
+    dndState = "idle";
   };
 </script>
 
 <main class="container">
   <h1>Drag and Drop Test</h1>
 
-  {#if dndState == "waiting"}
-    <div>Drag files here.</div>
-  {:else if dndState == "dragging"}
-    <div>Drop them here!</div>
-  {:else if dndState == "dropped"}
+  <div
+    class={["drop-area", dndState === "dragOver" && "drag-over"]}
+    bind:this={dropArea}
+  >
+    Drop here
+  </div>
+
+  <div>Status: {dndState}</div>
+  {#if dndState == "dropped"}
     <div>Files successfully dropped. Thank you! 🤗</div>
   {/if}
 
   {#if droppedPaths.length > 0}
-    <h2>Dropped File Path</h2>
     <ul>
       {#each droppedPaths as path}
         <li>{path}</li>
@@ -62,4 +98,16 @@
 </main>
 
 <style>
+  .drop-area {
+    width: 300px;
+    height: 200px;
+    border: 2px dashed #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+  .drag-over {
+    border-color: #007bff;
+  }
 </style>
